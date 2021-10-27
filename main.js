997fd6efbb99b9636e17e6eaa504fa3951276def
Maxvdkogel <maxvdkogel123@gmail.com>
2021-10-27 10:40:23 +0200

    added some details on mobile animation

--- main.js
+++ main.js
@@ -10,20 +10,31 @@ window.onload = () => {
       .to(".loaderName", {
           fontSize: 60,
           width: window.innerWidth -20,
-          duration: 1,
-          ease: "Power2.easeInOut"
+          duration: .75,
+          ease: "Power2.inOut"
       })
       .to(".cover", {
           top: document.querySelector(".header").offsetHeight + 15,
-          duration: 1,
-          ease: "Power2.easeInOut",
+          duration: .75,
+          ease: "Power2.inOut",
           onComplete: () => {
               document.querySelector(".cover").classList.add("invisible");
               document.querySelector(".name").classList.remove("invisible");
           }
-      }, "-=1")
-      .from(".circle, .jobTitle, .img1", {
-          opacity: 0,
-          duration: 1
-      })
+      }, "-=.75")
+      .from(".jobTitle", {
+            opacity: 0,
+            y: 100,
+            duration: .5,
+            ease: "power2.out"
+      }, "-=.4")
+      .from(".circle", {
+            opacity: 0,
+            duration: .5
+      }, "-=.5")
+      .from(".img1", {
+            y: document.querySelector(".img1").clientHeight,
+            duration: 1,
+            ease: "power2.out"
+    }, "-=.75");
 }
