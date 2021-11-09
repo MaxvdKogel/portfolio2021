2274ecda69e394e689d91d22137bfe19e0737b4c
Maxvdkogel <maxvdkogel123@gmail.com>
2021-11-09 22:43:12 +0100

    added a contact section, in view animation and a link hover effect

--- main.js
+++ main.js
@@ -1,9 +1,50 @@
 import './style.css'
 import gsap from 'gsap';
 
+//on refresh start at top
+let linkWasClicked = false;
+document.querySelector(".github").addEventListener("click", () => {
+    linkWasClicked = true;
+})
+document.querySelector(".mail").addEventListener("click", () => {
+    linkWasClicked = true;
+})
+
 window.onbeforeunload = function () {
-    window.scrollTo(0,0);
+    if(!linkWasClicked) {
+        window.scrollTo(0,0);
+        linkWasClicked = false;
+    }
 };
+
+window.addEventListener('scroll', () => {
+    var el = document.querySelectorAll(".hidden");
+    for (var i = 0; i < el.length; i++) {
+        if(el[i].getBoundingClientRect().top - (window.innerHeight * 0.75) < 0) {
+
+            el[i].classList.remove("hidden");
+
+            gsap.from(el[i], {
+                opacity: 0,
+                y: 50,
+                duration: .5
+            })
+
+        }
+    }
+})
+
+//reload page on resize to prevent bugs
+window.onresize = function(){ location.reload(); }
+
+//smooth scroll nav links
+document.querySelector(".projectLink").addEventListener("click", () => {
+    document.querySelector("#projects").scrollIntoView({behavior: "smooth", block:"start"}); 
+})
+
+document.querySelector(".navContactLink").addEventListener("click", () => {
+    document.querySelector("#contact").scrollIntoView({behavior: "smooth", block:"start"}); 
+})
 
 //loading animation
 
@@ -14,20 +55,20 @@ let imgMove = document.querySelector(".img1").clientHeight
 let containerPadding = 10;
 let extraSpacing = 0;
 
-if(window.screen.width <= 320) {
+if(window.innerWidth <= 320) {
     fontSize = 50;
 }
 
-if(window.screen.width > 600) {
+if(window.innerWidth > 600) {
     imgMove = imgMove * 1.5;
     containerPadding = 30;
 }
 
-if(window.screen.width > 900) {
+if(window.innerWidth > 900) {
     fontSize = 75;
 }
 
-if(window.screen.width > 1200) {
+if(window.innerWidth > 1200) {
     containerPadding = 100;
     fontSize = 100;
     extraSpacing = 7;
@@ -37,6 +78,7 @@ window.onload = () => {
     tl.to(".loaderName", { transform: "translate(0)", duration: 1, ease: "power2.out", onComplete: () => {
         document.querySelector(".cover").classList.add("disableCover");
         document.querySelector(".loader").style.backgroundColor = 'transparent';
+        document.querySelector(".loader").style.zIndex = "-1";
     } })
       .to(".loaderName", {
           fontSize: fontSize,
